a025db28b87befd7f2cb6418cd6c736dda09c639
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-10-14 19:43:46 -0600

    Fixed axios error

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
-    headers: {"ngrok-skip-browser-warning": "true"}
+  baseURL: "https://9dd4-50-117-239-144.ngrok-free.app",
+  headers: { "ngrok-skip-browser-warning": "true" },
 });
